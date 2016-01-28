e8254f1df93256699a815189313424c440e1cb5f
Perroud, Benoit <benoit@noisette.ch>
2016-01-28 21:19:08 +0100

    Initial commit

---
+++ src/main/java/ch/daplab/loglab/Spam.java
@@ -0,0 +1,49 @@
+package ch.daplab.loglab;
+
+import de.svenjacobs.loremipsum.LoremIpsum;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+/**
+ * Created by bperroud on 1/28/16.
+ */
+public class Spam implements Tool {
+
+    protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
+    private Configuration conf;
+    private LoremIpsum loremIpsum;
+    private boolean run = true;
+
+    public static void main(String[] args) throws Exception {
+        ToolRunner.run(new Spam(), args);
+    }
+
+    Random r = new Random();
+
+    @Override
+    public int run(String[] strings) throws Exception {
+
+        this.loremIpsum = new LoremIpsum();
+
+        while (run) {
+            LOG.warn(loremIpsum.getWords( r.nextInt(200) + 50, r.nextInt(5) + 1 ));
+        }
+
+        return 0;
+    }
+
+    @Override
+    public void setConf(Configuration configuration) {
+        this.conf = configuration;
+    }
+
+    @Override
+    public Configuration getConf() {
+        return conf;
+    }
+}
